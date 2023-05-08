ee5da8fa3a33b045f9f21a89500e5d491fd824c6
Gaspar Jimenez <oficialgaspii@gmail.com>
2023-05-08 19:48:39 -0400

    Nuevo Main

--- main.cpp
+++ main.cpp
@@ -3,13 +3,12 @@
 
 int main(){
 
-    ListArrLinked list(2);
+    int x;
+
+    ListArrLinked list(6);
 
     list.nuevo_nodo(list.raiz);
-    list.nuevo_nodo(list.raiz);
-    list.nuevo_nodo(list.raiz);
-    list.nuevo_nodo(list.raiz);
-    list.nuevo_nodo(list.raiz);
+    
     cout << list.raiz->b << "\n";
 
     list.insert_left(3);
@@ -17,10 +16,18 @@ int main(){
     list.insert_left(2);
     list.insert_left(4);
     list.insert_left(5);
-    list.insert(20, 3);
+    list.insert_right(6);
+    list.insert_left(7);
+    x=list.delete_right();
+   
+    
+
+    cout << "Se encuentra el valor 2 en la lista? " << (list.find(2) ? "Si" : "No") << endl; // Imprime Sí
+    cout << "Se encuentra el valor 4 en la lista? " << (list.find(4) ? "Si" : "No") << endl; // Imprime No
 
     list.print();
     cout<<"\n";
+    cout<<"eliminado:"<<x<<endl;
 
     cout << list.size() << "\n";
 
